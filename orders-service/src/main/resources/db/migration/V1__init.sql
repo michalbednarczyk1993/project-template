c80733bec029475e7e55fdf91cff967ddba4ecd8
michalbednarczyk1993 <michalbednarczyk1993@gmail.com>
2025-08-07 12:00:24 +0200

    Fix build-with-maven.yml and migrations

--- orders-service/src/main/resources/db/migration/V1__init.sql
+++ orders-service/src/main/resources/db/migration/V1__init.sql
@@ -1,16 +1,4 @@
-
-CREATE TABLE orders (
-    id UUID PRIMARY KEY,
-    user_id UUID NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    status VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE order_items (
-    id UUID PRIMARY KEY,
-    order_id UUID NOT NULL,
-    product_id UUID NOT NULL,
-    quantity INT NOT NULL,
-    price_per_unit NUMERIC(10,2) NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(id)
-);
+CREATE TABLE test_orders (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+); 
